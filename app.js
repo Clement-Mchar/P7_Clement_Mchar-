dc9193d7f4498f02f52cf4151401baff74c39299
Clement M'char <clement.mchar@gmail.com>
2022-04-12 19:52:49 +0200

    Création de l'api

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const userRoutes = require("./routes/user.routes");
+const postRoutes = require("./routes/post.routes");
+const { authCheck } = require("./middlewares/auth");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const { sequelize } = require("./models");
+
+
+async function main(){
+	await sequelize.sync({alter: true})
+
+}
+main()
+
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, DELETE, PATCH, OPTIONS"
+	);
+	res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
+	next();
+});
+app.get(bodyParser.json);
+//app.get("*", authCheck);
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+
+module.exports = app;
