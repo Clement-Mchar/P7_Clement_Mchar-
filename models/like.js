83f13244f5aefe3d67e77c90e7a1435b46fe505a
Clement M'char <clement.mchar@gmail.com>
2022-04-14 04:59:41 +0200

    Ajout du modèle like

--- models/like.js
+++ models/like.js
@@ -1,31 +1,37 @@
 "use strict";
 const { Model } = require("sequelize");
-module.exports = (sequelize, DataTypes) => {
+module.exports = (sequelize, Sequelize, DataTypes) => {
 	class like extends Model {
 		/**
 		 * Helper method for defining associations.
 		 * This method is not a part of Sequelize lifecycle.
 		 * The `models/index` file will call this method automatically.
 		 */
-		static associate({ post }) {
+		static associate({ post, user }) {
 			this.belongsTo(post, { foreignKey: "postId" });
+			this.belongsTo(user, { foreignKey: "userId" });
 		}
 	}
 	like.init(
 		{
+			id: {
+				type: Sequelize.UUID,
+				defaultValue: Sequelize.UUIDV4,
+				primaryKey: true,
+			},
 			userId: {
-				type: DataTypes.INTEGER,
-
+				type: Sequelize.UUID,
+				allowNull: false,
+				unique: true
 			},
 			postId: {
-				type: DataTypes.INTEGER,
-        
+				type: Sequelize.UUID,
+				allowNull: false,
 			},
 		},
 		{
 			sequelize,
 			modelName: "like",
-      tableName: "likes"
 		}
 	);
 	return like;
