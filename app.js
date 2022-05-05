cba933f34103fe99dd4b8a7455753cbbcdbb7cf9
Clement M'char <clement.mchar@gmail.com>
2022-05-05 20:41:21 +0200

    problème de cors

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL);
 	res.setHeader(
 	  "Access-Control-Allow-Headers",
 	  "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
